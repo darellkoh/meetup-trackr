e042ffe80159ab94576563e484497201b1a7703e
Darell Koh <darellkoh@gmail.com>
2016-11-27 15:51:00 -0500

    fixed server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-
+process.env.PWD = process.cwd();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: true }));
