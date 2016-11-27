e6420e832f0eafd00376066596d25bdedf04ef13
Darell Koh <darellkoh@gmail.com>
2016-11-27 16:10:05 -0500

    fixed server

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,22 @@
-const browserify = require('browserify');
-const gulp = require('gulp');
-const source = require("vinyl-source-stream");
-const reactify = require('reactify');
+// const browserify = require('browserify');
+// const gulp = require('gulp');
+// const source = require("vinyl-source-stream");
+// const reactify = require('reactify');
 
-gulp.task('browserify', () => {
-	const b = browserify({
-		entries: ['src/app.js'],
-		debug: true
-	});
-	b.transform(reactify); // use the reactify transform
-	return b.bundle()
-		.pipe(source('main.js'))
-		.pipe(gulp.dest('./dist'));
-});
+// gulp.task('browserify', () => {
+// 	const b = browserify({
+// 		entries: ['src/app.js'],
+// 		debug: true
+// 	});
+// 	b.transform(reactify); // use the reactify transform
+// 	return b.bundle()
+// 		.pipe(source('main.js'))
+// 		.pipe(gulp.dest('./dist'));
+// });
 
-gulp.task('watch', () => {
-	gulp.watch('src/*.js', ['browserify']);
-	gulp.watch('src/*.jsx', ['browserify']);
-});
+// gulp.task('watch', () => {
+// 	gulp.watch('src/*.js', ['browserify']);
+// 	gulp.watch('src/*.jsx', ['browserify']);
+// });
 
-gulp.task('default', ['watch', 'browserify']);
+// gulp.task('default', ['watch', 'browserify']);
