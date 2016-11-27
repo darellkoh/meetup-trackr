900554744d74f78a68a39da1bc8916b4b0727e2a
Darell Koh <darellkoh@gmail.com>
2016-11-27 15:08:05 -0500

    fixed server

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded( { extended: true }));
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/assets'));
 app.use(express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/node_modules'));
 app.use(express.static(__dirname + '/dist'));
 app.use(express.static(__dirname + '/src'));
 
